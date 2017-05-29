efaf85bd1088002d89936f3e1d7b1f4021c51a32
Tim Horton <hortont424@gmail.com>
2017-05-29 06:32:59 +0000

    fixes

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 
+fan_relay_channel = 13
+sensor_channel = 11
+
 GPIO.setup(fan_relay_channel, GPIO.OUT)
 GPIO.setup(sensor_channel, GPIO.IN)
 
 def read_sensor():
-    humidity, temperature = DHT.read_retry(DHT.AM2302, 11)
+    humidity, temperature = DHT.read_retry(DHT.AM2302, sensor_channel)
     return { "humidity": humidity, "temperature": temperature }
 
 def set_fan_state(state):
-    GPIO.output(13, state)
+    GPIO.output(fan_relay_channel, state)
 
-print read_sensor
+print read_sensor()
